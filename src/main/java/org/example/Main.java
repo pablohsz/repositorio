a85bfe4776b3b572cff34a6ca1cd1078f332acd0
pablohsz <pablosouza.dev@gmail.com>
2023-11-30 19:08:02 -0300

    criando método de adição

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int a, b;
+
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(adicao(a,b));
+    }
+
+    public static int adicao(int a, int b){
+        return a + b;
     }
 }
