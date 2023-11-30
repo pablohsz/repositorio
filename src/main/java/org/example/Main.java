864e4b32ef459d652871c42076abeec7d5057b20
pablohsz <pablosouza.dev@gmail.com>
2023-11-30 19:10:33 -0300

    criando método de adição com três números

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a, b;
+        int a, b, c;
 
         a = in.nextInt();
         b = in.nextInt();
-        System.out.println(adicao(a,b));
+        System.out.println(adicao(a, b));
+        c = in.nextInt();
+        System.out.println(adicao(a, b, c));
     }
 
-    public static int adicao(int a, int b){
+    public static int adicao(int a, int b) {
+        return a + b;
+    }
+
+    public static int adicao(int a, int b, int c) {
         return a + b;
     }
 }
